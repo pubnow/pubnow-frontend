93c33b18cc6c1a74f14d2c7d029033ab46405915
EvanNguyen <dacsang97@gmail.com>
2019-07-10 00:37:46 +0700

    feat(@pubnow/admin): add user info

--- packages/@pubnow/admin/components/commons/Navigation.vue
+++ packages/@pubnow/admin/components/commons/Navigation.vue
@@ -6,17 +6,30 @@
         <b-nav-item href="#">Dashboard</b-nav-item>
         <b-nav-item href="#">Statistic</b-nav-item>
       </b-navbar-nav>
+      <b-navbar-nav>
+        <b-nav-form>
+          <search-icon class="icon-2x" />
+          <input placeholder="Tìm kiếm" />
+        </b-nav-form>
+        <b-nav-item to="/notifications">
+          <notification-icon class="icon-2x notification" />
+        </b-nav-item>
+      </b-navbar-nav>
     </b-container>
     <user-info />
   </b-navbar>
 </template>
 
 <script>
+import NotificationIcon from 'vue-material-design-icons/Bell'
+import SearchIcon from 'vue-material-design-icons/Magnify'
 import UserInfo from './UserInfo'
 
 export default {
   components: {
     UserInfo,
+    NotificationIcon,
+    SearchIcon,
   },
 }
 </script>
@@ -39,6 +52,38 @@ export default {
 }
 .container-fluid {
   margin-left: $side-menu-w;
-  margin-right: $side-menu-w;
+  margin-right: $side-menu-w / 2;
+}
+
+.form-inline {
+  width: 200px;
+  height: 50px;
+  border-radius: 30px;
+  border: 2px solid $border;
+  overflow: hidden;
+  padding-left: 20px;
+  display: flex;
+
+  .material-design-icon {
+    color: $gray85;
+  }
+  input {
+    border: 0;
+
+    &:focus {
+      outline: 0;
+    }
+  }
+  ::placeholder {
+    color: $text;
+    font-size: 12px;
+    font-weight: 500;
+  }
+}
+
+.notification {
+  margin-top: 3px;
+  margin-left: 20px;
+  color: $gray85;
 }
 </style>
--- packages/@pubnow/admin/components/commons/UserInfo.vue
+++ packages/@pubnow/admin/components/commons/UserInfo.vue
@@ -1,3 +1,39 @@
 <template>
-  <div>User Area</div>
+  <div class="user-wrapper">
+    <div class="avatar" :alt="username" :style="`background-image: url(${avatar})`" />
+    <span class="username">{{username}}</span>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    avatar: {
+      type: String,
+      default:
+        'https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/32789851_1669417253172997_1743328170339205120_n.jpg?_nc_cat=110&_nc_oc=AQkOgwW6A37Y-TeD1CqaloQ4eQ6FnO3GWpjLX7IxdPlvGBmLMbCsYUjds7FC6QQ-HsE&_nc_ht=scontent.fhan2-4.fna&oh=020ca602d2d0ed83ea72bbca4c402984&oe=5DB936D6',
+    },
+    username: {
+      type: String,
+      default: 'dacsang97',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.user-wrapper {
+  display: flex;
+  align-items: center;
+  margin-right: 30px;
+  .avatar {
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    background-size: cover;
+  }
+  .username {
+    margin-left: 10px;
+  }
+}
+</style>
