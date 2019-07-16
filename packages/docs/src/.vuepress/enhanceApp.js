926ad18024db70609fc902116ba89d59d4e175f8
EvanNguyen <dacsang97@gmail.com>
2019-07-16 12:22:05 +0700

    feat(@pubnow/docs): build docs

--- packages/docs/src/.vuepress/enhanceApp.js
+++ packages/docs/src/.vuepress/enhanceApp.js
@@ -1,9 +1,1 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-import PubnowUI from '@pubnow/ui'
-import '@pubnow/ui/scss/pubnow-ui.scss'
-
-export default ({ Vue }) => {
-  Vue.use(PubnowUI)
-}
+export default ({ Vue }) => {}
