2035f96249b05e3e2cc7708089a630f0b59e0545
EvanNguyen <dacsang97@gmail.com>
2019-07-17 08:35:34 +0700

    chore(@pubnow/admin): change minibar color

--- packages/admin/components/commons/Navigation.vue
+++ packages/admin/components/commons/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <va-minibar class="minibar" theme="default" :top-items="minibarTopItems">
+    <va-minibar class="minibar" theme="white" :top-items="minibarTopItems">
       <div slot="top">
         <img :src="require('@/assets/images/logo.svg')" class="logo va-minibar-item-brand" />
         <div v-for="(item, index) in minibarTopItems" :key="index">
@@ -42,7 +42,7 @@ import AsideCreate from './AsideCreate'
 export default {
   components: {
     UserInfo,
-    AsideCreate
+    AsideCreate,
   },
   data() {
     return {
@@ -73,8 +73,8 @@ export default {
     },
     closeCreate() {
       this.$refs.myAsideCreate.close()
-    }
-  }
+    },
+  },
 }
 </script>
 
