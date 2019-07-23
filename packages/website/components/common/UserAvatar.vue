127735d6833573af250b36c309697371b6f6048e
EvanNguyen <dacsang97@gmail.com>
2019-07-24 00:56:41 +0700

    fix(@pubnow/website): usermenu ui

--- packages/website/components/common/UserAvatar.vue
+++ packages/website/components/common/UserAvatar.vue
@@ -11,22 +11,24 @@
         <va-button class="username" icon-after="angle-down" type="subtle-link">{{user.name}}</va-button>
       </div>
       <li v-for="(item, id) in getGroup" :key="id" class="organization-wrapper">
-        <img class="organization-avatar" v-if="item.avatar" :src="item.avatar" />
-        <div class="organization-name">{{item.name}}</div>
+        <nuxt-link :to="`/to-chuc/${item.slug}`" class>
+          <img class="organization-avatar" v-if="item.avatar" :src="item.avatar" />
+          <div class="organization-name">{{item.name}}</div>
+        </nuxt-link>
       </li>
       <li>
-        <a href="#">Xem thêm...</a>
-      </li>
-      <hr />
-      <li>
-        <a href=" /cai-dat/to-chuc">Quản lý nhóm</a>
-      </li>
-      <li>
-        <a href="/cai-dat">Cài đặt</a>
+        <nuxt-link to="/cai-dat/to-chuc">Xem thêm...</nuxt-link>
       </li>
       <hr />
       <li>
-        <a href="#" @click="logout">Đăng xuất</a>
+        <nuxt-link to="/cai-dat/to-chuc">Quản lý tổ chức</nuxt-link>
+      </li>
+      <hr />
+      <li>
+        <nuxt-link to="/cai-dat">Cài đặt</nuxt-link>
+      </li>
+      <li>
+        <a @click="logout">Đăng xuất</a>
       </li>
     </va-dropdown>
   </div>
@@ -42,7 +44,6 @@
 </template>
 
 <script>
-import shuffle from 'lodash.shuffle'
 import take from 'lodash.take'
 
 export default {
@@ -59,21 +60,25 @@ export default {
       organ: [
         {
           name: 'Vteam',
+          slug: 'vteam',
           avatar:
             'https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-0/p370x247/15542271_404549096543191_3973274007936829801_n.png?_nc_cat=110&_nc_oc=AQltokaG0AkEpOgNLkaS1LGq6hDoE7tjXNnTgNGeQvlLgy03jmwQHxxK8mVv9-myufs&_nc_ht=scontent.fhan2-4.fna&oh=033b1baa6ad4b16cd028562db20a666a&oe=5DA1B688',
         },
         {
           name: 'SSchedule',
+          slug: 'ssschedule',
           avatar:
             'https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-0/c0.0.370.370a/p370x247/60350132_459893864768521_5828017917681205248_n.jpg?_nc_cat=100&_nc_oc=AQlCum4cPzYvOC9X6xV6pVFdM7RIO2yHun9g-gZRiDRlJH2FTQkmMStKmBJRJpaP5Bo&_nc_ht=scontent.fhan2-4.fna&oh=a5922cfa60e627319819140610e97ae2&oe=5DB6D84A',
         },
         {
           name: 'Coralo Team',
+          slug: 'coralo',
           avatar: 'https://avatars0.githubusercontent.com/u/29174209?s=200&v=4',
           num: '30+',
         },
         {
           name: 'Young Tailor',
+          slug: 'yt',
           avatar: 'https://avatars0.githubusercontent.com/u/49083246?s=200&v=4',
         },
       ],
@@ -81,7 +86,7 @@ export default {
   },
   computed: {
     getGroup() {
-      return take(shuffle(this.organ), 3)
+      return take(this.organ, 3)
     },
   },
   methods: {
@@ -119,26 +124,29 @@ export default {
   }
   .dropdown-wrapper {
     margin-left: 10px;
-    background-color: #fff;
+    background-color: $white;
     .username {
       cursor: pointer;
     }
     li > a {
-      color: #505e77;
+      color: $n700;
     }
     .organization-wrapper {
       display: flex;
       flex-direction: row;
       align-items: center;
-      margin: 5px 10px;
-      .organization-avatar {
-        width: 24px;
-        border-radius: 12px;
-      }
-      .organization-name {
-        margin-left: 5px;
-        color: #505e77;
-        font-size: 14px;
+      a {
+        width: 100%;
+        justify-content: flex-start;
+        .organization-avatar {
+          width: 24px;
+          border-radius: 12px;
+        }
+        .organization-name {
+          margin-left: 5px;
+          color: $n700;
+          font-size: 14px;
+        }
       }
     }
   }
