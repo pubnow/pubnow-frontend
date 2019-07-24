3754e1a7600e119e47a3be19bf714cdcf36eff96
huongnguye1234 <huongnguyenthihuong1234@gmail.com>
2019-07-24 20:09:08 +0700

    feat(@pubnow/website): done comment

--- packages/website/pages/cai-dat/tai-khoan.vue
+++ packages/website/pages/cai-dat/tai-khoan.vue
@@ -1,85 +1,113 @@
 <template>
-  <div class="post-container">
-    <form class="form">
-      <div class="avatar">
-        <div class="image">
-          <img
-            src="https://s3-ap-southeast-1.amazonaws.com/img.spiderum.com/sp-xs-avatar/fc0e0530a6a611e9a9bf39a183ca3215.jpg"
-            class="rounded-circle z-depth-0"
-            width="70px"
-            height="70px"
-            alt
-          />
+  <no-ssr>
+    <div class="container">
+      <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+          <form class="form">
+            <div class="avatar">
+              <img
+                src="https://s3-ap-southeast-1.amazonaws.com/img.spiderum.com/sp-xs-avatar/fc0e0530a6a611e9a9bf39a183ca3215.jpg"
+                class="rounded-circle z-depth-0 image"
+                width="70px"
+                height="70px"
+                alt
+              />
+              <textarea name id cols="80" rows="2" class="text"></textarea>
+            </div>
+            <b-form class="pt-4 ip-form row">
+              <b-form-group id="input-group-1" class="form-cell col-6">
+                <label for class="name">User Name</label>
+                <p class="border-bottom">
+                  <span class="icon">
+                    <i class="fas fa-user-alt"></i>
+                  </span>
+                  <input placeholder="Write your name here.." class="pt-1" />
+                </p>
+              </b-form-group>
+              <b-form-group id="input-group-1" class="form-cell col-6">
+                <label for class="name">
+                  Email
+                  <a class="change" href="#s" @click="changeEmail()">
+                    <i class="fa fa-key"></i>
+                    thay đổi Email
+                  </a>
+                </label>
+                <p class="border-bottom">
+                  <span class="icon">
+                    <i class="far fa-envelope"></i>
+                  </span>
+                  <input v-if="display===true" placeholder="Write your name here.." />
+                  <label v-else class="pl-4">ggggggg</label>
+                </p>
+              </b-form-group>
+              <b-form-group id="input-group-1" class="form-cell col-6">
+                <label for class="name">Tên</label>
+                <p class="border-bottom">
+                  <span class="icon">
+                    <i class="fas fa-user-alt"></i>
+                  </span>
+                  <input placeholder="Write your name here.." class="pt-1" />
+                </p>
+              </b-form-group>
+              <b-form-group v-if="disPas===false" class="form-cell col-6">
+                <label for class="name">
+                  <a class="change1 pt-4" href="#s" @click="change(true)">
+                    <i class="fa fa-key"></i>
+                    thay đổi password
+                  </a>
+                </label>
+              </b-form-group>
+              <b-form-group v-if="disPas===true" class="form-cell w-100 change-pas">
+                <label for class="name">
+                  <a class="change1 pt-4" href="#s" @click="change(false)">
+                    <i class="fa fa-key"></i>
+                    thay đổi password
+                  </a>
+                </label>
+                <div class="d-inline-block float-left col-6">
+                  <label for class="name">mật khẩu cũ</label>
+                  <p class="border-bottom">
+                    <input class="pt-1 pl-0" />
+                  </p>
+                </div>
+                <div class="d-inline-block float-right col-6">
+                  <label for class="name">mật khẩu Mới</label>
+                  <p class="border-bottom">
+                    <input class="pt-1 pl-0" />
+                  </p>
+                </div>
+                <div class="d-inline-block float-right col-6 pt-4">
+                  <label for class="name">nhập lại mật khẩu Mới</label>
+                  <p class="border-bottom">
+                    <input class="pt-1 pl-0" />
+                  </p>
+                </div>
+              </b-form-group>
+              <b-button type="reset" variant="danger" class="btn-sub m-r b-text">Hủy</b-button>
+              <b-button type="submit" variant="primary" class="btn-sub b-text2">Lưu</b-button>
+            </b-form>
+          </form>
         </div>
-        <div class="text">
-          <textarea name id cols="74" rows="2"></textarea>
-        </div>
+        <div class="col-2"></div>
       </div>
-      <div class="form-cell">
-        <div>
-          <label for class="name">
-            User Name
-            <a class="change" href="#" @click="change()">
-              <i class="fa fa-key"></i>
-              thay đổi UserName
-            </a>
-          </label>
-          <p class="border-bottom">
-            <span class="icon">
-              <i class="fas fa-user-alt"></i>
-            </span>
-            <input v-if="display===true" placeholder="Write your name here.." class="pt-1" />
-            <label v-else class="pl-4">ggggggg</label>
-          </p>
-        </div>
-      </div>
-      <div class="form-cell">
-        <div>
-          <label for class="name">Tên</label>
-          <p class="border-bottom">
-            <span class="icon">
-              <i class="fas fa-user-alt"></i>
-            </span>
-            <input placeholder="Write your name here.." class="pt-1" />
-          </p>
-        </div>
-      </div>
-      <div class="form-cell">
-        <div>
-          <label for class="name">
-            Email
-            <a class="change" href="#s" @click="change()">
-              <i class="fa fa-key"></i>
-              thay đổi Email
-            </a>
-          </label>
-          <p class="border-bottom">
-            <span class="icon">
-              <i class="far fa-envelope"></i>
-            </span>
-            <input v-if="display===true" placeholder="Write your name here.." />
-            <label v-else class="pl-4">ggggggg</label>
-          </p>
-        </div>
-      </div>
-      <div class="btn-sub">
-        <div class="fl">
-          <a href="#" class="btn btn-danger btn-round">Hủy</a>
-          <input type="submit" value="lưu" class="btn btn-primary btn-round" />
-        </div>
-      </div>
-    </form>
-  </div>
+    </div>
+  </no-ssr>
 </template>
 <script>
 export default {
+  middleware: ['auth'],
   data() {
     return {
       display: false,
+      disPas: false,
     }
   },
   methods: {
-    change() {
+    change(value) {
+      this.disPas = value
+    },
+    changeEmail() {
       this.display = true
     },
   },
@@ -87,46 +115,49 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.name {
-  display: block;
-  width: 100%;
-  .change {
-    float: right;
-    color: #2fb5fa !important;
-  }
-}
-a :hover {
-  text-decoration: underline;
-  cursor: pointer;
-}
-.post-container {
+@import '@pubnow/ui/scss/_sizes.scss';
+@import '@pubnow/ui/scss/_colors.scss';
+@import '@pubnow/ui/scss/_mixins.scss';
+@import '@pubnow/ui/scss/_fonts.scss';
+
+.form {
   margin: auto;
-  width: 700px;
-  .form {
-    height: 500px;
-    padding: 20px 20px 20px 20px;
-    .avatar {
-      display: flex;
-      .image {
-        display: inline-block;
-      }
-      .text {
-        display: inline-block;
-        padding-left: 1.5rem;
-      }
-    }
-    .form-cell {
+  padding: $unit;
+  .change1 {
+    color: $aqua !important;
+  }
+  .avatar {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    .image {
       display: inline-block;
-      width: 49%;
-      padding-top: 20px;
-      padding-right: 20px;
-      padding-left: 0;
-      margin-bottom: 20px;
+    }
+    .text {
+      display: inline-block;
+      padding: 0 0 0 1.5rem;
+    }
+  }
+  .ip-form {
+    .form-cell {
+      padding: $unit;
       position: relative;
-      & p {
+
+      .name {
+        display: block;
+        .change {
+          float: right;
+          color: $aqua !important;
+        }
+        a :hover {
+          text-decoration: underline;
+          cursor: pointer;
+        }
+      }
+      p {
         height: 30px;
       }
-      & input {
+      input {
         border: 0;
         width: 100%;
         display: inline-block;
@@ -137,37 +168,51 @@ a :hover {
         z-index: 1;
         height: 20px;
         width: 20px;
-        left: 0px;
-        top: 55px;
-        font-size: 18px;
-        & i {
-          font-size: 18px;
+        left: 20px;
+        top: 54px;
+        font-size: 20px;
+        i {
+          font-size: 20px;
         }
       }
-      & input {
+      input {
         padding-left: 25px;
         height: 15px;
       }
     }
+    .change-pas {
+      background: $n10;
+      @include box-shadow;
+    }
     .btn-sub {
-      .fl {
-        float: right;
-        .btn {
-          width: 42% !important;
-          height: 34px !important;
-          line-height: 3 !important;
-          padding-left: 18px !important;
-          border-radius: 20px;
-        }
-        .btn-danger {
-          background-color: white;
-          color: red;
-        }
-        .btn-danger:hover {
-          background-color: red;
-          color: white;
-        }
-      }
+      height: 35px;
+      width: 60px;
+      text-align: center;
+      line-height: 2;
+      padding-left: 0px;
+      padding-right: 0;
+      border-radius: 27px;
+      font-weight: 400;
+      text-transform: lowercase;
+    }
+    .m-r {
+      margin: 0 1rem 0 34rem;
+    }
+    .b-text {
+      background-color: $n0;
+      color: $red;
+    }
+    .b-text:hover {
+      background-color: $red;
+      color: $n0;
+    }
+    .b-text2 {
+      background: $aqua !important;
+      border-color: $aqua !important;
+      color: $n0;
+    }
+    .b-text2:hover {
+      background: $primary !important;
     }
   }
 }
