e815e033dccb0ad262c80783099815df741f06ef
EvanNguyen <dacsang97@gmail.com>
2019-07-24 17:18:19 +0700

    fix(@pubnow/website): update organization setting

--- packages/website/pages/to-chuc/_orgname/quan-ly/cai-dat/index.vue
+++ packages/website/pages/to-chuc/_orgname/quan-ly/cai-dat/index.vue
@@ -1,75 +1,80 @@
 <template>
-  <b-container>
-    <b-row class="mt-3">
-      <b-col cols="md-8">
-        <va-form ref="form" type="vertical">
-          <va-form-item label="Tên tổ chức">
-            <va-input
-              :value="name"
-              @input="updateName"
-              name="name"
-              size="lg"
-              placeholder="Tên tổ chức"
-              :rules="[{type:'required', tip:'Bạn vui lòng nhập tên tổ chức'}]"
-            />
-          </va-form-item>
-          <va-form-item label="Email công khai">
-            <va-input
-              name="email"
-              :value="email"
-              @input="updateEmail"
-              size="lg"
-              placeholder="Email công khai"
-              :rules="[{type:'required', tip:'Bạn vui lòng nhập email'}, {type:'email', tip:'Bạn vui lòng nhập email'}]"
-            />
-          </va-form-item>
-          <va-form-item label="Mô tả">
-            <va-textarea :resize="false" :value="description" @input="updateDescription" />
-          </va-form-item>
-          <va-form-item>
-            <va-button
-              :active="!canUpdate"
-              :disabled="!canUpdate"
-              block
-              type="primary"
-              size="lg"
-              @click="submit"
-            >Chỉnh sửa</va-button>
-          </va-form-item>
-          <va-form-item>
-            <va-button block type="danger" size="lg" @click="removeOrgan">Xóa tổ chức</va-button>
-          </va-form-item>
-        </va-form>
-      </b-col>
-      <b-col cols="md-4" class="text-center">
-        <p class="text-profile mt-0">Profile picture</p>
-        <img
-          :src="imageUrl === '' ? 'https://bulma.io/images/placeholders/256x256.png': imageUrl"
-          alt="avatar to chuc"
-          class="avatar"
-        />
-        <span class="btn btn-outline-dark btn-file mt-3">
-          <input type="file" accept="image/*" @change="onFilePicked" />Upload new picture
-        </span>
-      </b-col>
-      <va-modal
-        title="Thông báo"
-        class="modal-container"
-        :backdrop-clickable="backdropClickable"
-        ref="customModal"
-      >
-        <div slot="body" class="mb-3">
-          <p class="content-rm">Bạn có chắc chắn muốn xóa tổ chức này không?</p>
-        </div>
-        <div slot="footer">
-          <div>
-            <va-button type="primary" @click="acceptRemove">Đồng ý</va-button>
-            <va-button @click="$refs.customModal.close()">Hủy bỏ</va-button>
+  <no-ssr>
+    <b-container>
+      <b-row class="mt-3">
+        <b-col cols="3" offset="1" class="text-center">
+          <p class="text-profile mt-0">Logo đại diện</p>
+          <img
+            :src="imageUrl === '' ? 'https://bulma.io/images/placeholders/256x256.png': imageUrl"
+            alt="avatar to chuc"
+            class="avatar"
+          />
+          <span class="va-btn va-btn-md va-btn-success btn-file mt-3">
+            <input type="file" accept="image/*" @change="onFilePicked" />
+            <div class="va-btn-text-fade" style="padding-left: 26px">
+              <va-icon type="upload" style="position: absolute;left: 3px;" />Upload ảnh
+            </div>
+          </span>
+        </b-col>
+        <b-col cols="md-7">
+          <va-form ref="form" type="vertical">
+            <va-form-item label="Tên tổ chức">
+              <va-input
+                :value="name"
+                @input="updateName"
+                name="name"
+                size="lg"
+                placeholder="Tên tổ chức"
+                :rules="[{type:'required', tip:'Bạn vui lòng nhập tên tổ chức'}]"
+              />
+            </va-form-item>
+            <va-form-item label="Email công khai">
+              <va-input
+                name="email"
+                :value="email"
+                @input="updateEmail"
+                size="lg"
+                placeholder="Email công khai"
+                :rules="[{type:'required', tip:'Bạn vui lòng nhập email'}, {type:'email', tip:'Bạn vui lòng nhập email'}]"
+              />
+            </va-form-item>
+            <va-form-item label="Mô tả">
+              <va-textarea :resize="false" :value="description" @input="updateDescription" />
+            </va-form-item>
+            <div class="va-form-group">
+              <div class="va-col-sm-12 va-flex justify-content-end">
+                <va-button
+                  :active="!canUpdate"
+                  :disabled="!canUpdate"
+                  type="primary"
+                  @click="submit"
+                  class="mr-1"
+                  icon-before="pen"
+                >Chỉnh sửa</va-button>
+                <va-button type="danger" @click="removeOrgan" icon-before="trash">Xóa tổ chức</va-button>
+              </div>
+            </div>
+          </va-form>
+        </b-col>
+        <va-modal
+          title="Thông báo"
+          class="modal-container"
+          :backdrop-clickable="backdropClickable"
+          ref="customModal"
+        >
+          <div slot="body" class="mb-3">
+            <p class="content-rm">Bạn có chắc chắn muốn xóa tổ chức này không?</p>
           </div>
-        </div>
-      </va-modal>
-    </b-row>
-  </b-container>
+          <div slot="footer">
+            <div>
+              <va-button type="primary" @click="acceptRemove">Đồng ý</va-button>
+              <va-button @click="$refs.customModal.close()">Hủy bỏ</va-button>
+            </div>
+          </div>
+        </va-modal>
+      </b-row>
+    </b-container>
+  </no-ssr>
 </template>
 
 <script>
@@ -143,6 +148,7 @@ export default {
 <style lang="scss" scoped>
 @import '@pubnow/ui/scss/_sizes.scss';
 @import '@pubnow/ui/scss/_colors.scss';
+@import '@pubnow/ui/scss/_mixins.scss';
 
 $size-image: 235px;
 
@@ -150,11 +156,12 @@ $size-image: 235px;
   width: $size-image;
   height: $size-image;
   border-radius: $size-image / 2;
+  @include box-shadow;
 }
 .text-profile {
   font-weight: 600;
   color: #5d6b83;
-  font-size: 14px;
+  font-size: 12px;
   margin-bottom: 8px;
 }
 
