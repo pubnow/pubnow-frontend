aadb81e2c05f1a7e743308c34719561d4ed1da9b
PhongPV <phamvanphong915@gmail.com>
2019-07-24 18:45:15 +0700

    feat(@pubnow/website): list followings

---
+++ packages/website/components/follow/author/CardAuthor.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="item-container d-flex mt-3 pb-3">
+    <div class="desc-container d-flex">
+      <img class="author-avatar" :src="url" alt />
+      <div class="content">
+        <div class="name">{{name}}</div>
+        <div class="desc">{{description}}</div>
+      </div>
+    </div>
+    <va-button type="primary">Đang theo dõi</va-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.item-container {
+  flex-direction: row;
+  align-items: center;
+  border-bottom-width: 1px;
+  border-bottom-color: #eee;
+  border-bottom-style: solid;
+  .desc-container {
+    flex-direction: row;
+    align-items: center;
+    .author-avatar {
+      width: 50px;
+      border-radius: 25px;
+    }
+    .content {
+      margin: 0 10px;
+      .name {
+        font-size: 14px;
+        color: #505e77;
+      }
+      .desc {
+        font-size: 13px;
+        color: #505e77;
+      }
+    }
+  }
+}
+</style>
+
---
+++ packages/website/components/follow/category/Category.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="categories mt-4">
+    <b-container>
+      <b-row>
+        <b-col v-for="(i,index) in listCategory" :key="index" sm="6" class="mb-4">
+          <CardCategory
+            :title="i.name"
+            :image="`https://source.unsplash.com/collection/1163637/480x480?sig=${index+1}`"
+          ></CardCategory>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+import CardCategory from './CardCategory'
+export default {
+  components: {
+    CardCategory,
+  },
+  computed: {
+    ...mapGetters({
+      listCategory: 'category/categories',
+    }),
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.d {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+</style>
+
