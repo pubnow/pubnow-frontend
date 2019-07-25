682a231e18a8e802f8abe373a838ffc9d08deacd
EvanNguyen <dacsang97@gmail.com>
2019-07-26 01:19:50 +0700

    feat(@pubnow/website): update timeRead filter

--- packages/website/plugins/common.js
+++ packages/website/plugins/common.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 
 Vue.filter('formatDate', source => {
   const date = new Date(source)
-  return date.getDate()
-    + ' tháng ' + (date.getMonth() + 1)
-    + ' năm ' + date.getFullYear()
+  return (
+    date.getDate() +
+    ' tháng ' +
+    (date.getMonth() + 1) +
+    ' năm ' +
+    date.getFullYear()
+  )
 })
 
 Vue.filter('timeRead', source => {
-  return Math.round(source.length / 600) + ' phút đọc'
+  const word = source.replace(/(<([^>]+)>)/gi, '').split(' ')
+  return Math.round(word.length / 220) + ' phút đọc'
 })
